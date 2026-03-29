fb323092e6174d8cbf9a34234d73948c6a4be16b
Elprofesor09 <toureyoussouf580@gmail.com>
2026-03-29 07:38:23 +0000

    fix import case

--- routes/cotisationExceptionnelleRoutes.js
+++ routes/cotisationExceptionnelleRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const CotisationExceptionnelle = require('../models/CotisationExceptionnelle');
-const Membre = require('../models/Membre');
 
 // POST - Ajouter une nouvelle cotisation spéciale
 router.post('/', async (req, res) => {
